727e21ee038afbcf80081424213e50a4582ae077
AyoubRavash <ayoubravash@gmail.com>
2024-01-08 12:18:00 +0330

    Creating Message class

---
+++ app/src/main/java/com/example/selfad/Message.kt
@@ -0,0 +1,17 @@
+package com.example.selfad
+
+import java.io.Serializable
+
+data class Message(
+    val contactName: String,
+    val contactNumber: String,
+    val myDisplayedName: String,
+    val includeJunior: Boolean,
+    val jobTitle: String,
+    val immediateStart: Boolean,
+    val startDate: String
+) : Serializable {
+
+    fun jobDescription() = if (includeJunior) "a junior $jobTitle" else "an $jobTitle"
+    fun startTime() = if (immediateStart) "immediately" else "from $startDate"
+}
